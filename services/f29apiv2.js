919d9a2687d8d09818fa87d3fa9b6c176f461927
f29 javi <javier.logrono@outlook.com>
2022-04-29 14:16:52 +0200

    added veriff added google login

--- services/f29apiv2.js
+++ services/f29apiv2.js
@@ -2,6 +2,8 @@
 
 const config = require('../config')
 const request = require('request')
+const key = config.TA_KEY;
+const endpoint = config.TA_ENDPOINT;
 
 function callTextAnalytics (req, res){
   var jsonText = req.body;
@@ -19,6 +21,24 @@ function callTextAnalytics (req, res){
   });
 }
 
+function callTextAnalyticsFhir (req, res){
+  var jsonText = req.body;
+  request.post({url:endpoint+'/text/analytics/v3.1-private-preview.5/entities/health/jobs/?structureFHIR=true',json: true,headers: {'Ocp-Apim-Subscription-Key': key},body:jsonText}, (error, response, body) => {
+    if (error) {
+      console.error(error)
+      res.status(500).send(error)
+    }
+    if(body=='Missing authentication token.'){
+      res.status(401).send(body)
+    }else{
+      console.log(response);
+      res.status(200).send(body)
+    }
+
+  });
+}
+
 module.exports = {
-	callTextAnalytics
+	callTextAnalytics,
+  callTextAnalyticsFhir
 }
