7c8fb8d7790a938ff671b2d94604d67964454f04
f29 javi <javier.logrono@outlook.com>
2022-09-28 11:33:54 +0200

    get group config

--- controllers/all/resources.js
+++ controllers/all/resources.js
@@ -10,17 +10,9 @@ function getQuestionnaire (req, res){
 
 async function getconfigFile(req, res) {
     let groupId= req.params.groupId;
-		let url = './raito_resources/groups/'+groupId+'/config.json';
-		let options = {json: true};
-		request(url, options, (error, res2, body) => {
-			if (error) {
-				res.status(404).send({message:'file not found'})
-			}else if(res2.body=='404: Not Found'){
-        res.status(404).send({message:'file not found'})
-      }else	if (!error && res2.statusCode == 200) {
-        res.status(200).send({body})
-			};
-		});
+	let url = './raito_resources/groups/'+groupId+'/config.json';
+	var json = JSON.parse(fs.readFileSync(url, 'utf8'));
+	res.status(200).send(json)
 }
 
 module.exports = {
