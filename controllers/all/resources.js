b02bebd98590ceaea198fa3c6b21302b561446ea
f29 javi <javier.logrono@outlook.com>
2022-11-23 16:20:31 +0100

    test 2

--- controllers/all/resources.js
+++ controllers/all/resources.js
@@ -644,7 +644,11 @@ function rateQuestionnaire(req, res) {
 		
 					var newavg = value/(ids.length)
 					json.rate = {avg:newavg, ids: ids}
-					fs.writeFile('./dist/raito_resources/questionnaires/' + req.body.id + '.json', JSON.stringify(json), (err) => {
+					fs.writeFile('./raito_resources/questionnaires/' + req.body.id + '.json', JSON.stringify(json), {
+						encoding: "utf8",
+						flag: "w+",
+						mode: 0o666
+					  }, (err) => {
 						if (err) {
 							console.log(req.body.id)
 							console.log(JSON.stringify(json));
