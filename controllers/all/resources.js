1b9f5c332a680cc328ddf0daf67bc0c45a9540d5
f29 javi <javier.logrono@outlook.com>
2022-11-24 09:08:53 +0100

    test

--- controllers/all/resources.js
+++ controllers/all/resources.js
@@ -613,66 +613,21 @@ async function getAllQuestionnaires(req, res) {
 }
 
 function rateQuestionnaire(req, res) {
-
-	var url = './raito_resources/questionnaires/' + req.body.id + '.json'
-	try {
-		var json = JSON.parse(fs.readFileSync(url, 'utf8'));
-		let groupId = req.params.groupId;
-		//verificar que el grupo existe
-		Group.findOne({ '_id': groupId }, function (err, group) {
-			if (err) return res.status(500).send({ message: `Error making the request: ${err}` })
-			if (!group) return res.status(404).send({ code: 208, message: 'The group does not exist' })
-			if(group){
-				
-				if (json.createdById != groupId) {
-					//subir file
-					var ids = json.rate.ids;
-					let found =false;
-					let value = 0;
-					for(var i=0;i<ids.length;i++){
-						if(req.params.groupId==ids[i].id){
-							found = true;
-							ids[i].value = req.body.value;
-						}
-						value = value + ids[i].value;
-					}
-		
-					if(!found){
-						ids.push({"id":req.params.groupId, "value": req.body.value})
-						value = value + req.body.value;
-					}
-		
-					var newavg = value/(ids.length)
-					json.rate = {avg:newavg, ids: ids}
-					fs.writeFile('./raito_resources/questionnaires/' + req.body.id + '.json', JSON.stringify(json), (err) => {
-						if (err) {
-							console.log(req.body.id)
-							console.log(JSON.stringify(json));
-							console.log(json);
-							console.log(err)
-							res.status(403).send({ message: 'not added' })
-						}
-		
-						res.status(200).send({ message: 'updated' })
-					});
-				} else {
-					res.status(200).send({ message: 'dont have permissions' })
-				}
-			}
-		})
-
-
-		
-
-
-	} catch (err) {
-		res.status(202).send({ message: 'dont exists' })
-		console.log(err);
+	var json = {"value": 1}
+		fs.writeFile('./dist/assets/raito_resources/questionnaires/' + req.body.id + '.json', JSON.stringify(json), (err) => {
+							if (err) {
+								console.log(req.body.id)
+								console.log(JSON.stringify(json));
+								console.log(json);
+								console.log(err)
+								res.status(403).send({ message: 'not added' })
+							}
+			
+							res.status(200).send({ message: 'updated' })
+						});
+	
+	
 	}
-
-
-
-}
 
 
 /**
