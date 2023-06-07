6f7dcf0187a4d14ea9dd4fc33429a00b92cf7069
f29 javi <javier.logrono@outlook.com>
2023-06-07 17:51:05 +0200

    finish step 1 inmuno

--- models/inmunodeficiencies.js
+++ models/inmunodeficiencies.js
@@ -7,11 +7,11 @@ const Patient = require('./patient')
 
 const { conndbdata } = require('../db_connect')
 
-const ImmunodeficienciesSchema = Schema({
+const InmunodeficienciesSchema = Schema({
 	data: {type: Object, default: []},
 	date: {type: Date, default: Date.now},
 	createdBy: { type: Schema.Types.ObjectId, ref: "Patient"}
 })
 
-module.exports = conndbdata.model('Immunodeficiencies',ImmunodeficienciesSchema)
+module.exports = conndbdata.model('Inmunodeficiencies',InmunodeficienciesSchema)
 // we need to export the model so that it is accessible in the rest of the app
