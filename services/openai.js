881b6595f5ead5a79bcd51119c7a21dc709d3729
Javier <javierlogronoaguirre@gmail.com>
2024-03-21 17:12:10 +0100

    change calls openai

--- services/openai.js
+++ services/openai.js
@@ -1,35 +1,35 @@
-const OpenAI = require('openai-api');
+const { Configuration, OpenAIApi } = require("openai");
 const config = require('../config')
 
 // Load your key from an environment variable or secret management service
 // (do not include your key directly in your code)
-const OPENAI_API_KEY = config.OPENAI_API_KEY;
 
-const openai = new OpenAI(OPENAI_API_KEY);
-
+const configuration = new Configuration({
+  apiKey: config.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
 
 function callOpenAi (req, res){
   //comprobar créditos del usuario
-  
   var jsonText = req.body.value;
+  var content = req.body.context;
+  var promt = 'Behave like a doctor. Returns only the name of the active ingredient of the following drug, nothing more: '+ jsonText;
   (async () => {
     try {
-        const gptResponse = await openai.complete({
-          engine: 'text-davinci-003',//davinci-instruct-beta-v3
-          prompt: jsonText,
-          maxTokens: 300,
-          temperature: 0,
-          topP: 1,
-          presencePenalty: 0,
-          frequencyPenalty: 0,
-          bestOf: 1,
-          n: 1,
-          stream: false
+      const gptResponse = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: [{role: "user", content:promt}, {role: "system", content: content}],
+        //prompt: jsonText,
+        temperature: 0,
+        max_tokens: 400,
+        top_p: 1,
+        frequency_penalty: 0,
+        presence_penalty: 0,
       });
       res.status(200).send(gptResponse.data)
     }catch(e){
       console.error("[ERROR]: " + e)
-      res.status(500).send(e)
+      res.status(500).send('error')
     }
     
   })();
